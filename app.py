41acdb20f04c0ebc26ae911a0c84086396f729a2
Chithra582 <chithu582@gmail.com>
2026-05-16 15:54:13 +0530

    Deploy fake account detector

--- app.py
+++ app.py
@@ -241,4 +241,4 @@ def index():
 
 if __name__ == '__main__':
     print("\n[ShieldAI] Fake Account Detection API running at http://localhost:5000")
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=7860)
